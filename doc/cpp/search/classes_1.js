0aaf415cb2a88d7bc44702e18c630a9090c8933b
Azure Pipelines <runner@Mac-1670606867846.local>
2022-12-09 18:04:24 +0000

    Updating C++ native SDK documentation

--- doc/cpp/search/classes_1.js
+++ doc/cpp/search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['builder_0',['Builder',['../structmillicast_1_1_audio_playback_1_1_builder.html',1,'millicast::AudioPlayback::Builder'],['../structmillicast_1_1_audio_source_1_1_builder.html',1,'millicast::AudioSource::Builder'],['../structmillicast_1_1_custom_source_1_1_builder.html',1,'millicast::CustomSource::Builder'],['../structmillicast_1_1_video_source_1_1_builder.html',1,'millicast::VideoSource::Builder']]]
+  ['bitratesettings_0',['BitrateSettings',['../structmillicast_1_1_bitrate_settings.html',1,'millicast']]],
+  ['builder_1',['Builder',['../structmillicast_1_1_audio_playback_1_1_builder.html',1,'millicast::AudioPlayback::Builder'],['../structmillicast_1_1_audio_source_1_1_builder.html',1,'millicast::AudioSource::Builder'],['../structmillicast_1_1_custom_source_1_1_builder.html',1,'millicast::CustomSource::Builder'],['../structmillicast_1_1_video_source_1_1_builder.html',1,'millicast::VideoSource::Builder']]]
 ];
--- doc/cpp/search/variables_2.js
+++ doc/cpp/search/variables_2.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['bit_5fdepth_0',['bit_depth',['../structmillicast_1_1rtcstats_1_1_video_stream_track.html#ac257c0bd15ba654420980e5724aac245',1,'millicast::rtcstats::VideoStreamTrack']]],
-  ['bits_5fper_5fsample_1',['bits_per_sample',['../structmillicast_1_1_audio_frame.html#accf60065352a5de15e4fb2cc049019a3',1,'millicast::AudioFrame']]],
-  ['bwe_2',['bwe',['../structmillicast_1_1_publisher_1_1_option.html#a48ab7332d4a8c930efeb62ad04566622',1,'millicast::Publisher::Option']]],
+  ['bitrate_5fsettings_1',['bitrate_settings',['../structmillicast_1_1_publisher_1_1_option.html#acdd69855e372251ca3b65abe3c994344',1,'millicast::Publisher::Option']]],
+  ['bits_5fper_5fsample_2',['bits_per_sample',['../structmillicast_1_1_audio_frame.html#accf60065352a5de15e4fb2cc049019a3',1,'millicast::AudioFrame']]],
   ['bytes_5freceived_3',['bytes_received',['../structmillicast_1_1rtcstats_1_1_inbound_rtp_stream.html#ab298467a2d247307cb063cace50c8322',1,'millicast::rtcstats::InboundRtpStream']]],
   ['bytes_5fsent_4',['bytes_sent',['../structmillicast_1_1rtcstats_1_1_sent_rtp_stream.html#a216c08eac6000e752f2378e3822db13b',1,'millicast::rtcstats::SentRtpStream']]]
 ];
